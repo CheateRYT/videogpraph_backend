81ac0012f37ffb357455739288880f486f8a1f2c
CheateRYT <cheaterytyt@gmail.com>
2025-01-30 22:47:09 +0300

    feat: cofigure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: configService.get<string>('TYPEORM_TYPE', 'postgres') as 'postgres',
+        host: configService.get<string>('TYPEORM_HOST', 'localhost'),
+        port: +configService.get<number>('TYPEORM_PORT', 5432), 
+        username: configService.get<string>('TYPEORM_USERNAME'),
+        password: configService.get<string>('TYPEORM_PASSWORD'),
+        database: configService.get<string>('TYPEORM_DATABASE'),
+        autoLoadEntities: configService.get<boolean>('TYPEORM_AUTOLOAD_ENTITIES', true),
+        synchronize: configService.get<boolean>('TYPEORM_SYNCHRONIZE', false),
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
